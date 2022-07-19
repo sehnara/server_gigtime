1ff7723be5c90be39831cf21812bb704f486b81f
sehnara <gsh723@naver.com>
2022-07-20 07:53:05 +0900

    test2

--- routes/owner/signup.js
+++ routes/owner/signup.js
@@ -73,7 +73,7 @@ signupRouter.post("/", getPos, async (req, res) => {
     const [sql4_result] = await con.query(sql4, sql2_result[0]['owner_id'])
 
     // console.log('sql4:', sql4_result);
-    // console.log('req:', req.body['store_jobs'][0]);
+    /// console.log('req:', req.body['store_jobs'][0]);
 
     const types = req.body['store_jobs'][0];  // 딕셔너리 배열
     const n = types.length;
@@ -94,7 +94,7 @@ signupRouter.post("/", getPos, async (req, res) => {
   } catch {
     console.log("error");
     con.release();
-    res.send("error-owner/signup");
+    res.send("error-owner/signup");//
   }
 });
 
